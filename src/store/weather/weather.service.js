b4ff2ec1ff2e4b077fa950c510131ba56743f2ee
Omri Steinberg <omristg@gmail.com>
2022-05-18 02:48:17 +0300

    deploy

--- src/store/weather/weather.service.js
+++ src/store/weather/weather.service.js
@@ -1,7 +1,6 @@
 import axios from "axios"
 
-// const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
-const API_KEY = 'j8mE5jvexnk05VSc8AZVDkqD5sbRgjn7'
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 // End Points
 const CURRENT_CONDITIONS_END_POINT = '/currentconditions/v1/'
