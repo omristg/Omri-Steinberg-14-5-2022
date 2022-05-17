927e23c3dec01d7404f796840626841237ba33d0
Omri Steinberg <omristg@gmail.com>
2022-05-18 02:23:41 +0300

    apikey change

--- src/store/weather/weather.service.js
+++ src/store/weather/weather.service.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
-// const API_KEY = 'j8mE5jvexnk05VSc8AZVDkqD5sbRgjn7'
+// const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+const API_KEY = 'j8mE5jvexnk05VSc8AZVDkqD5sbRgjn7'
 
 // End Points
 const CURRENT_CONDITIONS_END_POINT = '/currentconditions/v1/'
